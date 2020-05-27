60762dc533d1a113c1a5c3bb5ff5c43f4f02da04
tkchouaki <tkchouaki@icloud.com>
2020-05-27 13:26:10 +0200

    added my comments
    Signed-off-by: tkchouaki <tkchouaki@icloud.com>

--- controller.py
+++ controller.py
@@ -1,8 +1,12 @@
-#! C:\Users\USER\AppData\Local\Programs\Python\Python37-32\python.exe
+# No need for this line below
+# ! C:\Users\USER\AppData\Local\Programs\Python\Python37-32\python.exe
+
+# Please follow Pep reccomendations, I
+# f using Pycharm, you should have the warnings and you can fix them just by clicking on code -> reformat code
 import webview
 from model import Grid
-grid=Grid()
-w=webview.create_window("hello world",url="vue.html",js_api=grid)
+
+grid = Grid()
+w = webview.create_window("hello world", url="vue.html", js_api=grid)
 w.expose(grid.get_cell)
 webview.start()
-
